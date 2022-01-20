3451de3723cf23ed7d52a3789e8c438af9e2ca38
wcx <bethaniebell8474@gmail.com>
2022-01-21 00:03:38 +0800

    day 8_end_1 优化:优化文件类型对应的图标的与应用本身需要用于优化展示效果的图标的位置与获取方式. 添加:基本完成了应用的布局. 优化方向:统一对各种状态的保存集成为某个包处理或者使用启动包处理. 字节/字符文件查看器,以及设置默认打开,读取配置文件等.文件的复制等.

--- README.md
+++ README.md
@@ -1,4 +1,6 @@
 # File_explorer
-
+File_explorer for windows  
+设计原型依靠windows11文件浏览器,图标也拷贝自windows文件浏览器  
+用于学习之用  
 ![当前进度](https://images.gitee.com/uploads/images/2022/0117/165534_d5b0dea6_7528256.png "capture.png")
 
--- src/main/java/boostup/Utils.java
+++ src/main/java/boostup/Utils.java
@@ -4,4 +4,5 @@ public class Utils {
     public static int y = 39;//maybe for java1.8; 43 for java 17
     public static int x = 16;
     public static String load = "src\\main\\resources\\";
+//    public static String load = "";
 }
